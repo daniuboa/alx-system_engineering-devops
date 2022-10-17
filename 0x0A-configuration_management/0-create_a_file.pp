31e31571cc8970b472e5678fb3b12e447e59468c
Daniel Uboa <51877907+daniuboa@users.noreply.github.com>
2022-10-17 14:51:49 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# manifest to create a file in tmp directory
+# manifest to create file in tmp directory
 file { '/tmp/school':
-	ensure => present,
-	path => '/tmp/school',
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+  ensure  => present,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
